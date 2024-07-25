d91507695baac36bd69ddd60db32ad2514eceb6d
pedromourax <pedrolucas.moura@hotmail.com>
2024-07-25 16:27:13 -0300

    change tailwind & api returns

--- src/app/painel/[id]/page.tsx
+++ src/app/painel/[id]/page.tsx
@@ -3,7 +3,6 @@ import { ChangeEventHandler, useEffect, useState } from "react"
 import { cookies } from "next/headers"
 import { environment } from "@/app/enviroment"
 import Link from "next/link"
-import * as fs from 'fs'
 import UploadFile from "@/components/upload"
 
 export default async function groupoDetail( { params } ){
@@ -35,22 +34,20 @@ export default async function groupoDetail( { params } ){
 
     return (
         <div className="h-dvh flex flex-col justify-center items-center">
-            <Link href={'/painel'} className="absolute top-8 left-8 z-50 bg-slate-800 rounded-md p-2"> voltar </Link>
-            <div className="text-2xl font-extrabold p-4"> {retorno.grupo.nome} </div>
-            
+            <Link href={'/painel'} className="absolute top-8 left-8 z-50 bg-slate-800 rounded-md p-2"> grupos </Link>
+            <div className="text-2xl font-extrabold p-4 uppercase"> {retorno.grupo.nome} </div>
             <UploadFile token={retorno.token} grupoId={id}></UploadFile>
-            <div className="flex min-w-96 max-w-fit">
-                <div className="border-2 gap-2 border-white w-48  p-2">Nome</div>
-                <div className="border-2 gap-2 border-white w-48  p-2">Contato</div>
+            <div className="flex min-w-80 max-w-fit">
+                <div className="border-2 text-lg font-bold gap-2 border-white w-40  p-2">Nome</div>
+                <div className="border-2 text-lg font-bold gap-2 border-white w-40  p-2">Contato</div>
             </div>
             
             {retorno && retorno.usuarios.map(usuario => (
                 <div className="flex">
-                    <p className="border-2 gap-2 border-white w-48  p-2">{usuario.nome}</p>
-                    <p className="border-2 gap-2 border-white w-48  p-2">{usuario.contato.telefone}</p>
+                    <p className="border-2 gap-2 border-white w-40  p-2">{usuario.nome}</p>
+                    <p className="border-2 gap-2 border-white w-40  p-2">{usuario.contato.telefone}</p>
                 </div>
             ))}
-            
         </div>
     )
 }
